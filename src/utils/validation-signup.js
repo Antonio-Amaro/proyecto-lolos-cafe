e450b531104d2c15e31e1d3338470ecf0ffb7add
Antonio Amaro <ja.antonioamaro@gmail.com>
2026-01-18 21:55:17 -0600

    refactor: structure updated, files and folders names changed, html files moved to project root[DEV-AA]

--- src/utils/validation-signup.js
+++ src/utils/validation-signup.js
@@ -83,7 +83,7 @@ document.querySelector('.btn-submit').addEventListener('click', function(e) {
     console.log("Data saved for complete form");
     
     // Redirect to complete form
-    window.location.href = "./signupFull.html";
+    window.location.href = "./signup-full.html";
     
     return true;
 });
